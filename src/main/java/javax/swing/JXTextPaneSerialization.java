f3d900952c6c74ae8206884becf9614e70b04a3f
hofma103 <hofma103@users.noreply.github.com>
2017-05-06 22:03:35 +0200

    changed formatting to match the eclipse formatting (tabs instead of spaces, etc)

--- src/main/java/javax/swing/JXTextPaneSerialization.java
+++ src/main/java/javax/swing/JXTextPaneSerialization.java
@@ -10,13 +10,13 @@ import org.irsn.javax.swing.JXTextPane;
  */
 public class JXTextPaneSerialization extends JComponent {
 
-    private static final long serialVersionUID = -6908131063368800239L;
+	private static final long serialVersionUID = -6908131063368800239L;
 
-    public static byte getWriteObjCounter(final JComponent jc) {
-        return JComponent.getWriteObjCounter(jc);
-    }
+	public static byte getWriteObjCounter(final JComponent jc) {
+		return JComponent.getWriteObjCounter(jc);
+	}
 
-    public static void setWriteObjCounter(final JComponent jc, final byte count) {
-        JComponent.setWriteObjCounter(jc, count);
-    }
+	public static void setWriteObjCounter(final JComponent jc, final byte count) {
+		JComponent.setWriteObjCounter(jc, count);
+	}
 }
